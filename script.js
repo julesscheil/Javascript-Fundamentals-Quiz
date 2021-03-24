a01b32e7743bef8cd0e8556e4a76b035359b1d68
Jules Scheil <juliana.m.scheil@gmail.com>
2021-03-23 21:15:02 -0400

    Added button event listener and first question set up.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+//DOM Variables
+var questionContainer=document.getElementById("current-question");
+var nextQuestionButton=document.getElementById("next-question");
+
+//Javascript Variables
+
+var questionOne ={
+    question:"Question goes here",
+    choices: ["A","B","C","D"],
+    answers: "A",
+};
+
+//Function Definitions
+function displayQuestion() {
+    var questionEl = document.createElement("h1");
+    questionEl.textContent=questionOne.question;
+    questionContainer.append(questionEl);
+}
+//Event Listeners
+nextQuestionButton.addEventListener("click", function() {
+    console.log("You clicked the button");
+})
+//Function Calls
+displayQuestion();
