1f9f9a18bb01280cda4767961db25943dc06100e
Jules Scheil <juliana.m.scheil@gmail.com>
2021-03-24 23:35:28 -0400

    final changes, end game function and timer set

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Javascript-Fundamentals-Quiz
+# Description
+This is a coding quiz challenge. Click start quiz to start the timer, for each wrong answer, the timer will decrement 10 seconds. The quiz will not go to the next question until you select the correct answer.
+Here is an image of how the site looks: ![Portfolio  Site](./assets/images/screenshot.png)
+
+# Deployed Site
+You can find the link to the deployed site here: [Deployed Site](https://julesscheil.github.io/Javascript-Fundamentals-Quiz/)
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DOM Variables
 var questionContainer = document.getElementById("current-question");
 var nextQuestionButton = document.getElementById("next-question");
-
+var timeEl = document.querySelector(".time");
 
 // Javascript Variables
 
@@ -31,11 +31,29 @@ var arrayOfQuestions = [{
         answers: "console.log"
     }
 ]
-var answer = [];
+// index questions
 var currentIndex = 0;
+// timer second left
+var secondsLeft = 90;
 
 // Function Definitions
 
+// set time function to decrement timer
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if (secondsLeft <= 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+// function to display the question content
 function displayQuestion() {
     questionContainer.innerHTML = "";
     var questionEl = document.createElement("h1");
@@ -43,7 +61,7 @@ function displayQuestion() {
     // console.log(arrayOfQuestions[currentIndex].choices);
     questionContainer.append(questionEl);
 
-
+    // loop to create buttons as choices
     for (var i = 0; i < arrayOfQuestions[currentIndex].choices.length; i++) {
         console.log(i);
         var choicesEl = document.createElement("li");
@@ -51,56 +69,72 @@ function displayQuestion() {
         choicesEl.textContent = arrayOfQuestions[currentIndex].choices[i];
         choicesEl.setAttribute("type", "button");
         choicesEl.setAttribute("class", "btn btn-primary mybutton btn-block");
+        // If correct answer then listen for click to move to next question
         if (arrayOfQuestions[currentIndex].choices[i] == arrayOfQuestions[currentIndex].answers) {
             choicesEl.addEventListener("click", function () {
                 // currentIndex++;
                 console.log("You clicked the right button");
+                // if questions left then move to next question
                 if (currentIndex < arrayOfQuestions.length - 1) {
                     currentIndex++;
+                    // if no questions left, end game
                 } else {
+                    endTimer();
+                    endGame();
 
-                    return;
                 }
-                var wrongAnswerEl=document.getElementById("wrong-answer");
-                wrongAnswerEl.style.display="none";
+                // keep wrong answer hidden
+                var wrongAnswerEl = document.getElementById("wrong-answer");
+                wrongAnswerEl.style.display = "none";
                 displayQuestion();
             })
+            // otherwise if wrong answer, decrement timer and show wrong
         } else {
             choicesEl.addEventListener("click", function () {
-                var wrongAnswerEl=document.getElementById("wrong-answer");
-                wrongAnswerEl.style.display="block";
+                var wrongAnswerEl = document.getElementById("wrong-answer");
+                wrongAnswerEl.style.display = "block";
                 console.log("Wrong");
-                // decrement timer
+                secondsLeft -= 10;
+                if (secondsLeft === 0) {
+                    // Stops execution of action at set interval
+                    clearInterval(timerInterval);
+                    // Calls function to create and append image
+                    sendMessage();
+                }
             })
         }
-    
-    questionContainer.append(ulEl);
-    ulEl.append(choicesEl);
+        // Make buttons list and append to ul
+        questionContainer.append(ulEl);
+        ulEl.append(choicesEl);
+    }
 }
+
+// Game should show end game words when done
+function endGame() {
+    document.getElementById("quiz-content").style.display = "none";
+    document.getElementById("end-game").style.display = "block";
 }
-// if current index =5 then call end game function
 
-// end game function 
-// display page that has input to save initials 
-// should have access to timer as well to show timer
-// call end timer function
-// button that saves highscores - event listener
+function endTimer() {
 
-// end time function
-// clear interval function  (takes parameter named of set interval function)
+    secondsLeft = "";
 
-// set interval function 
-// place function towards top and call within displayfunction
-// timer -- 
+}
+
 
 // Event Listeners
+
+// Listen to start button to start timer and quiz
 document.getElementById("start-button").addEventListener("click", function () {
-    document.getElementById("start-page").style.display="none";
+    document.getElementById("start-page").style.display = "none";
     document.getElementById("quiz-content").style.display = "block";
-console.log("click");
+    console.log("click");
+    setTime();
     // displayQuestion();
 })
 
 
 // Function Calls
+
+// call questions to display from start
 displayQuestion();
